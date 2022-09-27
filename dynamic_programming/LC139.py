a56bb1761b0e68a15be2146c6b8f4097d42a86e6
zhoudexin <zhoudexin@bolean.com.cn>
2022-09-27 18:56:08 +0800

    update LC139

--- dynamic_programming/LC139.py
+++ dynamic_programming/LC139.py
@@ -3,3 +3,7 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # define dp[i] = last i characters  in wordDict
+        length = len(s)
+        dp = [False for i in range(length + 1)]
+        dp[0] = 0
